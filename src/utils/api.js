6908a298d97105f635230706766cdc01c403d43c
Protap Chakma <chakmaprotap855@gmail.com>
2026-08-09 12:55:17 +0600

    fix: resolve 404 API endpoint error for pets

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,9 @@
+// utils/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://pet-server-site.vercel.app/api',
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  }
+  baseURL: 'https://pet-server-site.vercel.app/api', 
+  withCredentials: true, 
 });
 
 export default api;
