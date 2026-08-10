2a478cedaee9921fa24ff81a54e34a7e09e4bf90
Protap Chakma <chakmaprotap855@gmail.com>
2026-08-10 16:52:19 +0600

    hobor no pang

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://pethouse-server-site.vercel.app',
+  baseURL: 'https://pethouse-server-site.vercel.app/api',
   withCredentials: true,
 });
 
